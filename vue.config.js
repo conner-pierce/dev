d82027d997e0f3e0b3568668f41a729e96f46847
Conner Pierce <cpiercework1@gmail.com>
2024-12-13 09:38:45 -0600

    Update publicPath in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/conner-pierce/dev/' // Replace 'your-repo-name' with your GitHub repository name
+    ? '/dev/' // Replace 'your-repo-name' with your GitHub repository name
     : '/',
   transpileDependencies: true,
   chainWebpack: config => {
